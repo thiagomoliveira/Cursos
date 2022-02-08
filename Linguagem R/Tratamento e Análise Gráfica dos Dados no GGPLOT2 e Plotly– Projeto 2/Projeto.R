44e7353301668e71f6880eef90ba281fde4f852c
Thiago Martins <pessoal.thiagomartins@gmail.com>
2022-02-08 07:23:53 -0300

    Curso de 'Analise de Dados com R' Finalizado

--- Linguagem R/Tratamento e Análise Gráfica dos Dados no GGPLOT2 e Plotly– Projeto 2/Projeto.R
+++ Linguagem R/Tratamento e Análise Gráfica dos Dados no GGPLOT2 e Plotly– Projeto 2/Projeto.R
@@ -1,6 +1,6 @@
 ### PROJETO 2 ###
 
-### Aula 1 - Tratamento inicial ###
+### Tratamento inicial ###
 
 library(dplyr)
 library(rstatix)
@@ -30,7 +30,7 @@ srag_sp_mod <- rename(srag_sp_mod, sexo = CS_SEXO, idade = NU_IDADE_N)
 sapply(srag_sp_mod, function(x) sum(is.na(x)))
 sapply(srag_sp_mod, function(x) sum(is.nan(x)))
 
-### Aula 2 - Análise dos dados: Gráfico de barras ###
+### Análise dos dados: Gráfico de barras ###
 
 help("graphics") # Pacote já vem instalado e inicializado
 
@@ -77,7 +77,7 @@ ggplot(srag_sp_mod, aes(x=CS_RACA)) +
                               x = "Raça", y = "Contagem")
 
 
-### Aula 3 - Análise dos dados: Gráfico de barras ###
+### Análise dos dados: Gráfico de barras ###
 
 sapply(srag_sp_mod, function(x) sum(is.na(x)))
 srag_sp_mod$CS_ZONA[which(is.na(srag_sp_mod$CS_ZONA))] <- 9
@@ -118,7 +118,7 @@ srag_sp_mod %>% plot_ly(x = ~ CS_RACA) %>%
                       layout(xaxis = list(title = "Raça"),
                              yaxis = list(title = "Quantidade"))
 
-### Aula 4 - Análise dos dados: Box Plot e outiliers ###
+### Análise dos dados: Box Plot e outiliers ###
 
 # Boxplot para idade
 
@@ -180,3 +180,206 @@ plot_ly(srag_atual, y = srag_atual$idade, color = srag_atual$sexo,
         type = "box") %>%
   layout(title = "BOXPLOT POR IDADE",
          xaxis = list(title = "Sexo"), yaxis = list(title = "Idade"))
+
+### Análise Estatística: Histograma, estatística descritiva e normalidade ###
+
+# Histograma para idade
+
+hist(srag_atual$idade, col = "blue", main = "SRAG POR IDADE",
+     xlab = "Distribuição das idades" , ylab = "Frequência")
+
+hist(srag_atual$idade, probability = T, col = "brown")
+lines(density(srag_atual$idade), col = "orange")
+
+## QQPLOT (GRÁFICO DE DISTRIBUIÇÃO NORMAL)
+qqnorm(srag_atual$idade, col = "gray")
+qqline(srag_atual$idade, col = "red")
+
+library(nortest)
+
+# Teste de normalidade Shapiro-Wilk
+shapiro.test(srag_atual$idade) #não vai rodar pelo tamanho da tabela
+
+# Anderson-Darling
+ad.test(srag_atual$idade)
+
+# Distribuição não é normal
+
+# HISTOGRAMA COM GG PLOT
+ggplot(data = srag_atual, aes(x = idade)) +
+  geom_histogram(fill = "red", bins = 25) + labs(title = "Histograma da idade",
+                                                 subtitle = "SRAG",
+                                                 x = "Idade", y = "Contagem")
+
+
+# HISTOGRAMA COM PLOTLY
+
+plot_ly(x = srag_atual$idade, type = "histogram") %>%
+  layout(title="HISTOGRAMA POR IDADE",
+         xaxix = list(title = "idade"), yaxis = list(title="Quantidade"))
+
+### Análise dos dados: Gráfico de dispersão e bolhas ###
+
+# Graficos de dispersão
+plot(srag_atual$DT_NOTIFIC, srag_atual$idade,
+     title("Casos de SRAG por mês e por idade"), col= "purple")
+
+scatter.smooth(srag_atual$DT_NOTIFIC, srag_atual$idade)
+
+# Gráfico de dispersão no ggplot (2 variáveis)
+ggplot(srag_atual, aes(x = DT_NOTIFIC, y = idade)) +
+  geom_point()+
+  labs(title = 'Relação data de nitificação e idade',
+       x = "Data de notificação",
+       y = "Idade")
+
+# Gráfico de dispersão no ggplot (4 variáveis)
+srag_atual_camp <- srag_atual %>% filter(ID_MN_RESI == "CAMPINAS")
+
+ggplot(srag_atual_camp, aes(x = DT_NOTIFIC, y = idade,
+                            color = CS_RACA, shape = sexo)) +
+  geom_point() +
+  labs(title = "Relação entre data de notificação, idade e sexo",
+       x = "Data de notificação",
+       y = "Idade")
+
+# Gráfico de dispersão no plotly (3 variáveis)
+plot_ly(x = srag_atual_camp$DT_NOTIFIC, y = srag_atual_camp$idade, type= "scatter",
+        mode = "markers", color = srag_atual_camp$sexo)
+
+# Gráfico de bolhas com ggplot
+srag_atual_tupa <- srag_atual %>% filter(ID_MN_RESI == "TUPA")
+ggplot(srag_atual_tupa, aes(x = DT_NOTIFIC, y = CS_ZONA,
+                            size = idade))+
+  geom_point() +
+  labs(title = "Relação entre data e região por idade",
+       x = "Data de notificação",
+       y = "Região")
+
+# Gráfico de bolhas com plotly
+plot_ly(x = srag_atual_camp$DT_NOTIFIC, y = srag_atual_camp$CS_ZONA, type = "scatter",
+        mode = "markers", size = srag_atual_camp$idade)
+
+# Análise dos dados: Gráfico de setores (pizza)
+
+table(srag_atual_camp$sexo)
+pie(table(srag_atual_camp$sexo), col=c("red", "blue"), radius= 2)
+
+glimpse(srag_atual_camp)
+srag_atual_camp$sexo <- as.factor(srag_atual_camp$sexo)
+
+contagem <- table(srag_atual_camp$sexo)
+nomes <- levels(srag_atual_camp$sexo)
+porcentagem <- round(contagem/sum(contagem)*100, 2)
+rotulo <- paste(nomes, " (", porcentagem, "%", ")", sep = " ")
+pie(table(srag_atual_camp$sexo), labels = rotulo, main = "SEXO",
+    col = c("red", "blue"), radius = 2)
+
+# Gráfico de setores com GGPLOT
+
+grafico_pizza <- ggplot(srag_atual_camp, aes(x=" ", fill = sexo))+
+  geom_bar(width = 1)+
+  coord_polar("y")
+grafico_pizza + theme(plot.background = element_rect(fill="grey", colour = "red"))
+
+grafico <- data.frame(
+  grupo = c("Masculino", "Feminino"),
+  valores = c(1311, 1041))
+soma = sum(table(srag_atual_camp$sexo))
+  
+grafico %>%
+  ggplot(aes(x = "", y = valores, fill = grupo)) +
+  geom_col() +
+  geom_text(aes(label = percent(valores/soma, accuracy = 0.1)), position = position_stack(vjust = 0.5)) +
+  scale_fill_brewer(palette = "Reds") +
+  coord_polar("y") +
+  theme_void() +
+  labs(title = "QUANTIDADE POR SEXO",
+       fill = "LEGENDA")
+
+install.packages("scales")
+library(scales)
+
+# Gráfico de setores com Plotly
+plot_ly(srag_atual_camp, labels = ~ sexo, type = "pie")
+plot_ly(srag_atual_camp, labels = ~ CS_RACA, type = "pie")
+plot_ly(srag_atual_camp, labels = ~ CS_ZONA, type = "pie")
+
+
+# Distribuição de Frequências ###
+
+install.packages("sampling")
+install.packages("TeachingSampling")
+library(sampling)
+library(TeachingSampling)
+
+# Tabela de Frequências absolutas
+freq_abs <- table(srag_atual$idade)
+View(freq_abs)
+
+# Tabela de Rrequências relativas
+freq_rel <- prop.table(freq_abs)
+View(freq_rel)
+
+# Porcentagem da Frequência relativa
+p_freq_rel <- 100*prop.table(freq_rel)
+View(p_freq_rel)
+
+# Criar linha com o total
+freq_abs <- c(freq_abs, sum(freq_abs))
+View(freq_abs)
+names(freq_abs)[112] <- "Total"
+
+# Juntando a Frequência relativa com a frequência percentual com as suas respectivas somas
+freq_rel <- c(freq_rel, sum(freq_rel))
+p_freq_rel <- c(p_freq_rel, sum(p_freq_rel))
+
+# Tabela final com todos os valores
+tabela_final <- cbind(freq_abs,
+                      freq_rel = round(freq_rel, digits = 5),
+                      p_freq_rel = round(p_freq_rel, digits = 2))
+
+View(tabela_final)
+
+# Construindo classes de frequências
+intervalos_classes <- seq(0, 120,10)
+View(intervalos_classes)
+tabela_classes <- table(cut(srag_atual$idade, breaks = intervalos_classes, right = FALSE))
+View(tabela_classes)
+
+# Gráficos de frequência
+
+# Histograma 
+hist(srag_atual$idade, col = "red")
+
+df1 <- as.data.frame(tabela_classes)
+
+df1 %>% plot_ly(x = ~Var1, y = ~Freq) %>%
+  layout(xaxis = list(title = "Intervalo de idades"),
+         yaxis = list(title = "Quantidade"))
+
+# Poligono de frequência
+plot(tabela_classes, type = 'o')
+?plot
+
+# Gráfico de Ogiva
+freq_rel_classes <- prop.table(table(cut(srag_atual$idade,
+                                         breaks = c(intervalos_classes))))
+View(freq_rel_classes)
+freq_acum <- cumsum(tabela_classes)[seq_along(intervalos_classes)]
+View(freq_acum)
+
+# Gráfico
+plot(intervalos_classes, freq_acum, type = 'o')
+
+# Gráfico de Ogiva
+df <- as.data.frame(freq_acum)
+
+ggplot(df, aes(x = intervalos_classes, y = freq_acum)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "GRÁFICO DE OGIVA: FREQUÊNCIA ACUMULADA POR CLASSES DE IDADE",
+       y = "idade",
+       y = "Frequência acumulada de SRAG",
+       color = "Meses") +
+  theme_gray()
